80c3d310bfd37bc118b61a050c71673ff7957992
David Tran <davidttemail@gmail.com>
2020-02-26 10:17:51 -0700

    Create market_test with tests for attributes

---
+++ test/market_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/vendor'
+require './lib/item'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+end
